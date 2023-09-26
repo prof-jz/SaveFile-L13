cc62c525807537e5d95dbd3d44d495f7f12f1034
prof-jz <111508888+prof-jz@users.noreply.github.com>
2023-09-26 15:36:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() 
+{
+  string fileInput, usrInput;
+
+  //JZ - initialize your file data streams & open the files here
+
+
+  
+  while(!reader.eof())
+    {
+      //JZ - input the code to read the next line of text from 'reader' into fileInput
+
+
+      
+      cout << "\t" << fileInput;
+    }
+
+  cout << "Please write a sentence that you'd like to write to file: ";
+  getline(cin, usrInput);
+ 
+  //JZ - write the line of code that places the contents of usrInput into the output stream 'writer'
+  
+}
